03cf7a0ce980d6d5f90a264c5afb7d8dcd29834f
samirzjadhav <samirzjadhav@gmail.com>
2025-10-07 13:34:24 +0530

    feat: added structure of hangman on it

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import { useState } from "react";
 import "./App.css";
 import words from "./wordList.json";
+import HangmanDrawing from "./components/HangmanDrawing";
+import Keyboard from "./components/Keyboard";
+import HangmanWord from "./components/HangmanWord";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
-  
+
+  const [guessedLetters, setGuessedletters] = useState<string[]>([]);
+
   console.log(wordToGuess);
 
-  return <></>;
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div className="" style={{ fontSize: "2rem", textAlign: "center" }}>
+        Lose Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
