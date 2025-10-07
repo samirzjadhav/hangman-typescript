5a42215cc508b8183417a6d2c48afd0cab7e45ec
samirzjadhav <samirzjadhav@gmail.com>
2025-10-07 13:44:07 +0530

    feat: aded hangmanword on it

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const HangmanWord = () => {
+  const word = "test";
+  const guessedLetters = ["t", "e"];
   return (
-    <div>
-      
+    <div
+      style={{
+        display: "flex",
+        gap: "2.5rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default HangmanWord
+export default HangmanWord;
